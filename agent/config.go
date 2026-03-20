bf24a7b47c5f1fde04c6f04edeb65ed948a11f81
douaoouria rassim <162766373+Rassimdou@users.noreply.github.com>
2026-03-20 01:18:45 +0100

    add the config

---
+++ agent/config.go
@@ -0,0 +1,55 @@
+package agent
+
+import (
+	"bytes"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type AgentConfig struct {
+	ID string `yaml:"id"` //'auto' means detect hostname at runtime
+}
+
+type ServerConfig struct {
+	Address string `yaml:"address"`
+	TLS     bool   `yaml:"tls"`
+}
+
+type WatchConfig struct {
+	Paths     []string `yaml:"paths"`
+	Recursive bool     `yaml:"recursive"`
+}
+type EventConfig struct {
+	Include []string `yaml:"include"`
+}
+type ScanConfig struct {
+	OnReconnect bool `yaml:"on_reconnect"`
+}
+
+// the root struct
+type Config struct {
+	Agent  AgentConfig  `yaml:"agent"`
+	Server ServerConfig `yaml:"server"`
+	Watch  WatchConfig  `yaml:"watch"`
+	Events EventConfig  `yaml:"events"`
+	Scan   ScanConfig   `yaml:"scan"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+
+	decoder := yaml.NewDecoder(bytes.NewReader(data))
+	decoder.KnownFields(true)
+
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
